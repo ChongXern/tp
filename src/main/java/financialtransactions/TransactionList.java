83163ef97518363fa9aa9d3a22bb8726a39b2913
ChongXern <chongxern@gmail.com>
2024-03-30 01:48:11 +0800

    Remove unused collections import

--- src/main/java/financialtransactions/TransactionList.java
+++ src/main/java/financialtransactions/TransactionList.java
@@ -1,7 +1,6 @@
 package financialtransactions;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class TransactionList<T extends Transaction<?>> {
