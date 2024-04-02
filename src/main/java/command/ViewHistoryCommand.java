ace737fd3a4f181171dcbac6de4cfd0a66b93381
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2024-04-02 11:16:18 +0800

    Add all in view history command to view everything

--- src/main/java/command/ViewHistoryCommand.java
+++ src/main/java/command/ViewHistoryCommand.java
@@ -10,15 +10,13 @@ public class ViewHistoryCommand extends BaseCommand {
 
     public String execute(TransactionManager manager) throws Exception{
         //@@author Kishen271828
-        String numTransactionsString = null;
-        for (String part : commandParts) {
-            if (part.startsWith("n/")) {
-                numTransactionsString = part.substring(2);
-            }
-        }
+        //String numTransactionsString = null;
         int numTransactions = 0;
-        if (numTransactionsString != null) {
+        if (commandParts[1].startsWith("n/")) {
+            String numTransactionsString = commandParts[1].substring(2);
             numTransactions = Integer.parseInt(numTransactionsString);
+        } else if (commandParts[1].equals("all")) {
+            numTransactions = manager.getTransactionListSize();
         }
         return manager.showLastNTransactions(numTransactions);
     }
